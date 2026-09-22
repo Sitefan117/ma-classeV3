1bde37835a20ba7442b6f2d3647c87ab7bc4d451
Sitefan117 <stefan.gomesdesousa@proton.me>
2026-09-22 08:46:10 +0200

    fix prerender error for teacher route in svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,17 +3,26 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/ma-classeV3' : ''
-		}
-	}
+    preprocess: vitePreprocess(),
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html'
+        }),
+        paths: {
+            base: process.env.NODE_ENV === 'production' ? '/ma-classeV3' : ''
+        },
+        prerender: {
+            handleHttpError: ({ path, referrer, message }) => {
+                // Ignore les erreurs de prerender pour les formulaires de la zone teacher
+                if (path.startsWith('/teacher')) {
+                    return;
+                }
+                throw new Error(message);
+            }
+        }
+    }
 };
 
 export default config;
